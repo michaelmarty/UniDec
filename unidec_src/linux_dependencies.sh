5683a7ca1b12b8c7f2de1b487c2c680b4761eecc
michaelmarty <michael.marty@chem.ox.ac.uk>
2016-03-01 08:52:56 +0000

    added linux_dependiencies.sh

---
+++ unidec_src/linux_dependencies.sh
@@ -0,0 +1,13 @@
+# To Run GUI
+sudo apt-get install python-pip
+sudo apt-get install python-scipy
+sudo pip install pymzml twython networkx natsort pyinstaller
+sudo apt-get install python-matplotlib
+sudo apt-get install python-wxgtk3.0
+
+# To build C code from source
+sudo apt-get install libfftw3-dev
+
+# To build with pyinstaller
+sudo apt-get install libgdk-pixbuf2.0-dev
+sudo apt-get install python-requests-oauthlib
