dc616d7bb9862c989d02550397302b9331c0b105
Andrew Norris <norris.702@osu.edu>
2020-06-16 13:16:29 -0400

    Updates to the nmsgsb_report
    Edited the report format. Added hyperref for boxes.

--- unidec_modules/thermo_reader/rawreader.py
+++ unidec_modules/thermo_reader/rawreader.py
@@ -6,18 +6,18 @@ import time
 def get_raw_metadata(path):
     rawfile = MSFileReader(path)
 # Most are columns in the Xcalibur sequence editor
+# "Company" - Research Group name
+    company = rawfile.GetSeqRowUserText(index=3)
+    print(company)
 # "Laboratory" - collaborator PI's name
-    lab = rawfile.GetSeqRowUserLabel(index=2)
-    lab1 = lab.replace('Laboratory', '')
-    print(lab1)
-
+    lab = rawfile.GetSeqRowUserText(index=2)
+    print(lab)
 # "Client" - collaborator's name.
-    client = rawfile.GetSeqRowUserLabel(index=1)
-    client1 = client.replace('Client', '')
-    print(client1)
+    client = rawfile.GetSeqRowUserText(index=1)
+    print(client)
 # "Phone" - This will be "shipment date"
-    phone = rawfile.GetSeqRowUserLabel(index=4)
-    shipment = phone.replace('Phone', 'Shipment Date: ')
+    phone = rawfile.GetSeqRowUserText(index=4)
+    shipment = phone.replace('Phone', '')
     print(shipment)
 # Andrew - The date modified = "Data recorded" - this date is the end of the run.
     # Get the time of last modifation of the specified path since the epoch
@@ -29,28 +29,39 @@ def get_raw_metadata(path):
     inst = rawfile.GetInstModel()
     print(inst)
 # "Study" - could be solution info - OBE mM AmAc
-    study = rawfile.GetSeqRowUserLabel(index=0)
-    study1 = study.replace('Study', 'Study: ')
-    print(study1)
+    study = rawfile.GetSeqRowUserText(index=0)
+    print(study)
 # Injection would be useful to add to our reports
     inject = rawfile.GetSeqRowInjectionVolume()
     print(inject)
-
     
 
-    output_string="Client: " + str(lab1)+ "- " + str(client1) + ";" + str(shipment) + ";" + "Data Recorded: " + str(local_time) + ";" + "Instrument: " + str(inst) + ";" + str(study1) + ";" + "Injection (uL): " + str(inject)
-    print(output_string)
+    output_string="Lab: " + str(company) + ";" +"Client: " + str(lab)+ " - " + str(client) + ";" + "Shipment Date: " + str(shipment) + ";" + "Data Recorded: " + str(local_time) + ";" + "Instrument: " + str(inst) + ";" + "Study: " + str(study) + ";" + "Injection (uL): " + str(inject)
+    #print(output_string)
     return output_string
 
 
 if __name__ == "__main__":
-    dir = "C:\\Users\\norri\\OneDrive\\Desktop"
-    dir = "D:\\Data"
-    filename = "AN_20191122_UHMR_nESI_RedBeta_RB_01uM_A38_04uM_50AA.raw"
+    dir = "C:\\Users\\norri\\OneDrive\\Desktop\\UniDec_report tests"
+    #dir = "D:\\Data"
+    filename = "20200603_Ivan_D3-5A2_3k_HCD005.raw"
     path = os.path.join(dir, filename)
     get_raw_metadata(path)
 
-    
+def get_raw_samplename(path):
+    rawfile = MSFileReader(path)
+# "sample name" - will be in the title of the report
+    rawsamplename = rawfile.GetSeqRowSampleName()
+    print(rawsamplename)
+    output_string= str(rawsamplename)
+    return output_string
+
+if __name__ == "__main__":
+    dir = "C:\\Users\\norri\\OneDrive\\Desktop\\UniDec_report tests"
+    #dir = "D:\\Data"
+    filename = "20200603_Ivan_D3-5A2_3k_HCD005.raw"
+    path = os.path.join(dir, filename)
+    get_raw_companyname(path)
     
     #for i in range(rawfile.FirstSpectrumNumber, rawfile.LastSpectrumNumber + 1):
     #    print(i, rawfile.GetCollisionEnergyForScanNum(i, MSOrder=1))
