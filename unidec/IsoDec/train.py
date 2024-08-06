75eed0a86ee87fc3f0c35db988a2c3a75816b5f2
michaelmarty <martymt10@gmail.com>
2024-08-05 17:10:08 -0700

    Updated HPC train

--- unidec/IsoDec/train.py
+++ unidec/IsoDec/train.py
@@ -9,14 +9,20 @@ import os
 # Short script for training on an HPC
 os.chdir("/xdisk/mtmarty/mtmarty/training/data")
 
-eng = IsoDecEngine(phaseres=2)
+eng = IsoDecEngine(phaseres=4)
 topdirectory = "/xdisk/mtmarty/mtmarty/training/data"
 dirs = [os.path.join(topdirectory, d) for d in data_dirs]
-eng.create_merged_dataloader(dirs, "phase2", noise_percent=0, batchsize=32, double_percent=0)
-eng.train_model(epochs=5)
-eng.create_merged_dataloader(dirs, "phase2", noise_percent=0.0, batchsize=32, double_percent=0.2)
+eng.create_merged_dataloader(dirs, "phase4", noise_percent=0, batchsize=32, double_percent=0)
 eng.train_model(epochs=10)
-eng.create_merged_dataloader(dirs, "phase2", noise_percent=0.05, batchsize=32, double_percent=0)
+eng.create_merged_dataloader(dirs, "phase4", noise_percent=0.0, batchsize=32, double_percent=0.2)
+eng.train_model(epochs=10)
+eng.create_merged_dataloader(dirs, "phase4", noise_percent=0.05, batchsize=32, double_percent=0)
+eng.train_model(epochs=10)
+
+eng.create_merged_dataloader(dirs, "phase4", noise_percent=0.02, batchsize=32, double_percent=0.05)
+eng.train_model(epochs=10)
+eng.train_model(epochs=10, lossfn="weightedcrossentropy")
+eng.train_model(epochs=10, lossfn="focal")
 eng.train_model(epochs=10)
 
 
