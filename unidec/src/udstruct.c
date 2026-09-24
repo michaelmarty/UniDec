2ef898b80c1577a24d80bf51db47945db0b38f77
michaelmarty <mtmarty@utexas.edu>
2026-09-24 11:50:58 -0500

    Slight updates to prepare for release

--- unidec/src/udstruct.c
+++ unidec/src/udstruct.c
@@ -435,7 +435,7 @@ void PrintHelp() {
     printf("\t\t\t\t0=Integration\n");
     printf("\t\t\t\t1=Interpolation\n");
     printf("\t\t\t\t2=Smart blend of interpolation and integration\n");
-    printf("\nEnjoy! Please report bugs to Michael Marty (mtmarty@utexas.edu) commit date 9/9/2026\n");
+    printf("\nEnjoy! Please report bugs to Michael Marty (mtmarty@utexas.edu) commit date 9/24/2026\n");
     //printf("\nsize of: %d",sizeof(char));
 
     /*
